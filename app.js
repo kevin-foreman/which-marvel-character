cefccae851eaa72281b2dfb873ecefa8b9a20192
kevin-foreman <kevin.o.foreman2@gmail.com>
2023-02-08 15:04:08 -0600

    Updated with .env so I can protect my private API key

--- app.js
+++ app.js
@@ -1,11 +1,16 @@
 let characterSelected, searchTerm;
+require('dotenv').config();
+let myAPIKey = process.env.privateApiKey;
 
 $('#characterPick').on('click', function () {
+    console.log("I got clicked!")
     let searchTerm = $('#userAnswer')
 });
 
 function characterSelect() {
 
+    // Toggle the 'hide' class on the input field so we can reveal it
+    // to the user once questions are complete
 
 
 };
