e016b42996c090444ea8125e93c34ffaf0c56edc
kevin-foreman <kevin.o.foreman2@gmail.com>
2023-02-09 10:44:40 -0600

    Formatting the API call

--- app.js
+++ app.js
@@ -7,34 +7,16 @@ let userDosier = 0;
 
 $('#characterPick').on('click', function () {
     console.log("characterPick got clicked!");
-    let searchTerm = $('#userAnswer').val();
+    let characterSearch = $('#userSearch').val();
 
-    // prompt("Yes or No");
-    function dosierPrompt1() {
-        let dosierPrompt1 = confirm("Are you prone to mistakes?");
-        if (dosierPrompt1) {
-            userDosier += 1;
-            alert("You said yes!");
-        } else {
-            userDosier += 2;
-            alert("You said no :(");
-        };
-    };
-
-    dosierPrompt1();
-
-});
-
-function characterSelect() {
-
-    $.get(`https://gateway.marvel.com:443/v1/public/characters?apikey=34ccc8ffaeb21342da2868e7bf73270a
-    `, (data) => {
+    // md5sum 61802d3189a7f5e71f01372764ebd2ea
+    $.get(`https://gateway.marvel.com:443/v1/public/characters?name=${characterSearch}apikey=34ccc8ffaeb21342da2868e7bf73270a`, (data) => {
         console.log(data);
         let arrayData = JSON.stringify(data);
         let resultObj = JSON.parse(arrayData);
     });
-    // Toggle the 'hide' class on the input field so we can reveal it
-    // to the user once questions are complete
 
 
-};
+    // dosierPrompt();
+
+});
