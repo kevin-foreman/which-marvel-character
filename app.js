f7ff4dafb7e1b9bfb69920e7c5445f2e2a4192f6
kevin-foreman <kevin.o.foreman2@gmail.com>
2023-02-09 11:15:19 -0600

    Added md5 hashing for API keys

--- app.js
+++ app.js
@@ -4,13 +4,19 @@ let characterSelected, searchTerm;
 // console.log(process.env) // remove this after you've confirmed it is working
 
 let userDosier = 0;
+let ts = Date.now();
+let myPrivateAPIKey = 'a4591843cae8c4fbe86dc71edf01ba695df3bb2a'; // process.env.privateApiKey;
+let myPublicAPIKey = '34ccc8ffaeb21342da2868e7bf73270a'; // process.env.publicApiKey;
+let stringToHash = ts + myPrivateAPIKey + myPublicAPIKey;
+let hashValue = md5('stringToHash');
+
 
 $('#characterPick').on('click', function () {
     console.log("characterPick got clicked!");
     let characterSearch = $('#userSearch').val();
 
     // md5sum 61802d3189a7f5e71f01372764ebd2ea
-    $.get(`https://gateway.marvel.com:443/v1/public/characters?name=${characterSearch}apikey=34ccc8ffaeb21342da2868e7bf73270a`, (data) => {
+    $.get(`https://gateway.marvel.com:443/v1/public/characters?name=${characterSearch}&${ts}&apikey=${myPublicAPIKey}&hash=${hashValue}`, (data) => {
         console.log(data);
         let arrayData = JSON.stringify(data);
         let resultObj = JSON.parse(arrayData);
